ca31224e146c6a3e84e4579046dd229bad8f62ab
Jared Clifton <jaredcraigclifton@gmail.com>
2013-10-07 19:09:00 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'backbone-on-rails'
+gem 'devise'
+
+group :development do
+  gem 'sqlite3'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+  gem 'launchy'
+  gem 'letter_opener'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# authentication/authorization
+gem 'bcrypt-ruby', :require => 'bcrypt'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChessApplication::Application.config.secret_token = '1c51b673ec1a7ae1278b732520981855c26faddcb6f5479069672bba239f82fc4b889aa77afc7a5dec3e4753e09f9d19d1f56e0dc8c645a07adaebc4f0be4599'
