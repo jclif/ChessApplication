23c55eca4cade35cd1c0211d28b5a3cc0f6bf653
Jared Clifton <jaredcraigclifton@gmail.com>
2013-10-20 22:56:29 -0400

    stylin

--- app/assets/javascripts/views/games_list.js
+++ app/assets/javascripts/views/games_list.js
@@ -23,7 +23,37 @@ ChessApplication.Views.GamesListView = Backbone.View.extend({
   render: function() {
     var that = this;
 
+    var is_white = function(uni) {
+      whites = [
+        "\u2654",
+        "\u2655",
+        "\u2656",
+        "\u2657",
+        "\u2658",
+        "\u2659"];
+
+      return _.contains(whites, uni)
+    };
+
+    var white_to_solid = function(uni) {
+      var white_to_black = {
+      "\u2655": "\u265B",
+      "\u2654": "\u265A",
+      "\u2656": "\u265C",
+      "\u2657": "\u265D",
+      "\u2658": "\u265E",
+      "\u2659": "\u265F"
+      };
+
+      if (_.contains(Object.keys(white_to_black), uni)) {
+        return white_to_black[uni];
+      }
+    };
+
+
     that.$el.html(that.template({
+      is_white: is_white,
+      white_to_solid: white_to_solid,
       games: that.collection
     }));
 
